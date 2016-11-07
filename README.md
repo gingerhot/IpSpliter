3c087530d3491f5b033f80444c07fc69c2854e57
B1nj0y <v@joyv.net>
2016-11-07 23:06:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## A quiz from Vincross's interview
+
+Split a string like "2502501135" to get all valid ip addresses as possible:
+
+["250,250,1,135","250,250,11,35","250,250,113,5"]
---
+++ ipSpliter.hs
@@ -0,0 +1,28 @@
+--
+-- by idegorepl<at>gmail.com
+-- run by "runghc ipSpliter.hs" in shell
+-- before making sure you've installed a Haskell runtime like GHC
+--
+
+import Data.List (intercalate, tails)
+
+ipSpliter :: String -> [String]
+ipSpliter s = [ intercalate "," x | x <- allIps, validCheck x ]
+    where validCheck = all (\x -> (read x :: Int) < 256 && checkHeadZero x)
+          checkHeadZero x = if length x > 1 then head (map (:[]) x) /= "0" else True
+          allSplitPos = combinations 3 [1..length s - 1]
+          allIps = map (\x -> splitIp x s) allSplitPos
+
+splitIp :: [Int] -> String -> [String]
+splitIp [] s = [s]
+splitIp (x:xs) s = m : splitIp (map (subtract x) xs) n
+    where (m,n) = splitAt x s
+
+combinations :: Int -> [Int] -> [[Int]]
+combinations 0 _ = [[]]
+combinations n xs = [ y:ys | y:xs' <- tails xs, ys <- combinations (n-1) xs']
+
+main :: IO ()
+main = do
+    let s = "2502500135"
+    putStrLn $ show $ ipSpliter s
